1c57333b17c445841bb8690712319de9fbbff61c
VictorTrume <vhtrume@gmail.com>
2025-05-06 11:37:52 -0600

    [AGREGAR] nombre en publicaciones y comentarios.

--- PIW/php/registro.php
+++ PIW/php/registro.php
@@ -1,31 +1,37 @@
 <?php
 //No olvidar modificar esta parte cuando se suba a la nube
-$host = 'localhost' ;
+$host = 'localhost';
 $user = 'root';
-$password ='';
+$password = '';
 $dbname = 'directorioweb';
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
-if ($conn->connect_error){
+if ($conn->connect_error) {
     die("Error de conexion: ". $conn->connect_error);
 }
 
 if (isset($_POST['registro'])) {
-    $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
-    $contraseña = $_POST['contraseña'];
+    // Escapar las entradas para evitar inyección SQL
+    $nombre = $conn->real_escape_string($_POST['nombre']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $contraseña = $conn->real_escape_string($_POST['contraseña']);
 
+    // Crear el hash de la contraseña
     $contraseña_hash = password_hash($contraseña, PASSWORD_BCRYPT);
 
-    $sql = "INSERT INTO usuarios (nombre, email, contraseña) VALUES ('$nombres', '$email', '$contraseña_hash')";
+    // Insertar la nueva entrada en la base de datos
+    $sql = "INSERT INTO usuarios (nombre, email, contraseña) VALUES ('$nombre', '$email', '$contraseña_hash')";
 
-    if ($conn->query($sql) === TRUE){
+    if ($conn->query($sql) === TRUE) {
         echo "¡¡Registro exitoso!! <a href='../Inicio_Sesion.html'>Iniciar Sesion </a>";
-    } else{
+    } else {
         echo "Error: " .$sql . "<br>" .$conn->error;
     }
 
-    $conn->close;
+    $conn->close();
 }
+?>
+<?php
+echo "El archivo registro.php se está ejecutando.";  // Agrega esta línea para depuración
 ?>
